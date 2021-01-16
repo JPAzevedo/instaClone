7d1d6b79c91cdb1820e1059d310486a7c3e97270
System Administrator <JPAzevedo90@outlook.pt>
2021-01-16 17:55:12 +0000

    Adding fragments to the app

--- app/src/main/java/com/jpazevedo/instaclone/MainActivity.kt
+++ app/src/main/java/com/jpazevedo/instaclone/MainActivity.kt
@@ -1,54 +1,62 @@
 package com.jpazevedo.instaclone
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
+import com.jpazevedo.instaclone.fragments.HomeFragment
+import com.jpazevedo.instaclone.fragments.NotificationsFragment
+import com.jpazevedo.instaclone.fragments.ProfileFragment
+import com.jpazevedo.instaclone.fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
     // Check lateinit here: https://www.bignerdranch.com/blog/kotlin-when-to-use-lazy-or-lateinit/
-    private lateinit var message: TextView
+    //private lateinit var message: TextView
 
-    // This is a functional interface with only one argument. So, the lambda function can be called this way: interfaceName { (arg) -> logic of the function }
+    internal var selectedFragement : Fragment? = null
+    internal var frameLayout : Int? = 0
+
+
+    // This is a functional interface with only one argument. So, the lambda function can be called this way: interfaceName { arg -> logic of the function }
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
         item -> when(item.itemId){
             R.id.nav_home -> {
-                message.setText("Home")
-                return@OnNavigationItemSelectedListener true
+                selectedFragement = HomeFragment()
             }
             R.id.nav_search -> {
-                message.setText("Search")
-                return@OnNavigationItemSelectedListener true
+                selectedFragement = SearchFragment()
             }
             R.id.nav_add_post -> {
-                message.setText("Post")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications -> {
-                message.setText("Notifications")
+                selectedFragement = NotificationsFragment()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_profile -> {
-                message.setText("Profile")
+                selectedFragement = ProfileFragment()
                 return@OnNavigationItemSelectedListener true
             }
+            else -> return@OnNavigationItemSelectedListener false
         }
-        false
+        supportFragmentManager.beginTransaction().replace(frameLayout!!,selectedFragement!!).commit()
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        message = findViewById(R.id.message)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        frameLayout = R.id.fragment_container
+        supportFragmentManager.beginTransaction().replace(frameLayout!!,HomeFragment()).commit()
 
     }
+
 }
+
+
